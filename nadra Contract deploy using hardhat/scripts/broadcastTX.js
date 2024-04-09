2c5a81913677c310e22e25766bb65d7a48ee4cbe
Zeeshan Nawaz <106486240+zeeshanmnawaz786@users.noreply.github.com>
2024-04-09 11:14:12 +0500

    Update broadcastTX.js

--- nadra Contract deploy using hardhat/scripts/broadcastTX.js
+++ nadra Contract deploy using hardhat/scripts/broadcastTX.js
@@ -1,9 +1,9 @@
 // Transaction set on BLockchain 
 
 var ethers =require('ethers');
-const INFURA_API_URL = 'https://rinkeby.infura.io/v3/4b23bef4af964a72af90e99ec8c93f21';
-var PRIVATE_KEY='2e911f9576c27017615d69f5c6bb54ca89be7170d0f11cdba67478c4f88c94eb';
-const CONTRACT_ADDRESS = '0xdd6db3d632c8677d9DAF4C950bc8EE1D0599188b';
+const INFURA_API_URL = 'paste your infura api';
+var PRIVATE_KEY='paste your private api';
+const CONTRACT_ADDRESS = 'paste your contract address';
 const CONTRACT_ABI = require('../artifacts/contracts/nadra.sol/NADRA.json');
 
 let customHttpProvider = new ethers.providers.JsonRpcProvider(INFURA_API_URL);
